3912178bcc590f1a71eb82afc3fe0899b70878f2
Vladyan/ <145528782+GangurSunduck@users.noreply.github.com>
2023-09-21 13:52:29 +0300

    Add files via upload

---
+++ ИТ-20/Сундуков-Завьялов/05.09.2023СундуковЗавьяловБатонОнКлик/js/js2.js
@@ -0,0 +1,25 @@
+"use strict"
+function myFunction() {
+    var x = document.getElementById("myForm").elements[0].value;
+    document.getElementById("pole_vivoda").innerHTML = x;
+
+    var x1 = document.getElementById("myForm").elements[1].value;
+    document.getElementById("pole_vivoda2").innerHTML = x1;
+
+    var x2 = document.getElementById("myForm").elements[2].value;
+    document.getElementById("pole_vivoda3").innerHTML = x2;
+
+    var x3 = document.getElementById("myForm").elements[3].value;
+    document.getElementById("pole_vivoda4").innerHTML = x3;
+
+    var x4 = document.getElementById("myForm").elements[4].value;
+    document.getElementById("pole_vivoda5").innerHTML = x4;
+
+    var x5 = document.getElementById("myForm").elements[5].value;
+    document.getElementById("pole_vivoda6").innerHTML = x5;
+
+    var x6 = document.getElementById("myForm").elements[6].value;
+    document.getElementById("pole_vivoda7").innerHTML = x6;
+
+}
+ 
